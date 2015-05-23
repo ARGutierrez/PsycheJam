5155154c187355c845eded8d1f795cf7dd128b6e
Vincent Wing <vwing@uci.edu>
2015-05-23 11:10:25 -0700

    Added Sixense. Added player body.

---
+++ Assets/Scripts/FollowBall.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowBall : MonoBehaviour {
+	public Transform ball;
+	private float x;
+	private float y;
+	private float z;
+
+	void Awake()
+	{
+
+	}
+	void Start () 
+	{
+		x = transform.position.x - ball.position.x;
+		y = transform.position.y - ball.position.y;
+		z = transform.position.z - ball.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.position = new Vector3(ball.position.x + x, ball.position.y + y, ball.position.z + z);
+	}
+}
