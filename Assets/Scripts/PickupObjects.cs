5acc7e1530cbcb15663842099f9912a7d8378c4a
Kevin Wong <kgwong1@uci.edu>
2015-05-23 14:58:26 -0700

    Pickuppable Objects and Level0 creation

---
+++ Assets/Scripts/PickupObjects.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupObjects : MonoBehaviour {
+
+    private int numObjects = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(numObjects);
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PickUpObject"))
+        {
+            other.GetComponent<PickedUp>().onPickUp();
+            numObjects++;
+        }
+    }
+}
