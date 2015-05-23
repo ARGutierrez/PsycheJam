27de3a2d32cd33730657e21efa09eec1c50ff4bd
Aaron Gutierrez <argutierrez92@gmail.com>
2015-05-22 23:21:46 -0700

    Player and headtracking

---
+++ Assets/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+
+	private GameObject m_currentTarget;
+	public GameObject Target
+	{
+		get { return m_currentTarget; }
+		set 
+		{
+			m_currentTarget = value;
+		}
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
---
+++ Assets/Scripts/Interaction/HeadTrackSelection.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeadTrackSelection : MonoBehaviour 
+{
+	private Player m_player;
+
+	Vector3 m_trackingPoint; 
+
+	// Use this for initialization
+	void Start () 
+	{
+		m_player = GetComponent<Player> ();
+
+		// m_trackingPoint = GameObject.Find ("CenterEyeAnchor").transform.position;
+		if (m_trackingPoint == null) {
+			m_trackingPoint = this.transform.position;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		RaycastHit hit;
+		if (Physics.Raycast (m_trackingPoint, transform.forward, out hit)) 
+		{
+			Debug.DrawRay(m_trackingPoint, transform.forward, Color.red);
+			Debug.Log("test");
+		}
+	}
+}
