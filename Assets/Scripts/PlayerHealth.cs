8f0f66d5979e049375f9bdc783004cd7efeea7b6
Kevin Wong <kgwong1@uci.edu>
2015-05-23 19:05:50 -0700

    Added player death and game over

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+    public int maxHealth = 100;
+    public int currentHealth;
+
+	// Use this for initialization
+	void Start () {
+        currentHealth = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Enemy"))
+        {
+            takeDamage(5);
+        }
+    }
+
+    public void takeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+            gameOver();
+    }
+
+    public void gameOver()
+    {
+        Application.LoadLevel("GameOver");
+    }
+}
