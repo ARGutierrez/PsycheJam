89d16a06311cf622da02dbd8c1718adf1423fb70
Kevin Wong <kgwong1@uci.edu>
2015-05-23 17:09:48 -0700

    Added a bunch of prefabs

---
+++ Assets/Scripts/MoveTowardTarget.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTowardTarget : MonoBehaviour {
+
+    public Transform target;
+    public float speed;
+
+    void Update()
+    {
+        move();
+    }
+
+    void move()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
---
+++ Assets/Scripts/SpawnMonster.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnMonster : MonoBehaviour {
+
+    public GameObject monster;
+    public float timeBetweenSpawn;
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("spawn", 0f, timeBetweenSpawn);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void spawn()
+    {
+        Instantiate(monster, transform.position, Quaternion.identity);
+    }
+}
